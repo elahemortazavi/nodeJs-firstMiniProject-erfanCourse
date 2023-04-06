299b0ff5b2b2702a634ab458e624b2bb72010b02
elahemortazavi <emor5680@gmail.com>
2023-04-06 12:38:08 +0100

    data

---
+++ server.js
@@ -0,0 +1,8 @@
+const http =  require("http");
+const PORT = 3000;
+const server = http.createServer((req, res) => {
+
+})
+
+server.listen(PORT);
+console.log(`run server on port ${PORT} http://localhost${PORT}`)
