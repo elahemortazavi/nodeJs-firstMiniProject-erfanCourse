0ffb0439561338c9f4a4a3d740f9cc93dd9eb9ff
elahemortazavi <emor5680@gmail.com>
2023-04-08 11:17:40 +0100

    /api/products

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const http =  require("http");
+const products = require ("./data/products.json");
 const PORT = 3001;
 const server = http.createServer((req, res) => {
+    if(req.url == "/api/products") {
+        res.writeHead(200, {'content-Type':'application.json'});
+        res.write(JSON.stringify(products))
+        res.end()
+    }else{
+        res.writeHead(404, {'content-Type': 'application.json'});
+        res.write(JSON.stringify({
+            message: "route not found"
+        }))
+        res.end()
+    }
 
 })
 
